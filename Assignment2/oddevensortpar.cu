2d39c3c542533e172058aebca692734dd5356ba4
Victor <victarv@gmail.com>
2022-12-05 21:35:52 +0100

    Assignment 2 part 1 done?

--- Assignment2/oddevensortpar.cu
+++ Assignment2/oddevensortpar.cu
@@ -14,15 +14,13 @@ void swap(int *a, int *b) {
 
 __global__
 void oddEvenSort_kernel(int* numbers_d, int n, int stride) {
-    int t_idx = threadIdx.x + blockDim.x*blockIdx.x;
-    if (t_idx*stride*2 >= n) return;
+    int t_idx = (threadIdx.x + blockDim.x*blockIdx.x)*2;
+    if (t_idx >= n) return;
     for (int i = 0; i <= n; i++) {
-        int idx = t_idx*stride*2;
+        int idx = t_idx;
         idx += (i+1) % 2 == 1 ? 1 : 0;
-        for (int j = idx; j < idx + stride*2 && j < n-1; j += 2) {
-            if (numbers_d[j] > numbers_d[j + 1]) {
-                swap(&numbers_d[j], &numbers_d[j + 1]);
-            }
+        for (int j = idx; j < n; j += stride) {
+            numbers_d[j] > numbers_d[j + 1] ? swap(&numbers_d[j], &numbers_d[j + 1]) : NULL;
         }
         __syncthreads();
     }
@@ -37,7 +35,7 @@ void oddeven_sort(std::vector<int>& numbers)
     int threads_per_block = 1024;
 
     int* numbers_d;
-    int stride = (int)std::ceil(s/(float)threads_per_block);
+    int stride = threads_per_block*2;
     auto s_bytes = s*sizeof(int);
     cudaMalloc((void**)&numbers_d, s_bytes);
     cudaMemcpy(numbers_d, &numbers[0], s_bytes, cudaMemcpyHostToDevice);
@@ -45,19 +43,11 @@ void oddeven_sort(std::vector<int>& numbers)
     std::cout <<"Error: " << cudaGetErrorString(cudaGetLastError()) << std::endl;
     cudaMemcpy(&numbers[0], numbers_d, s_bytes, cudaMemcpyDeviceToHost);
     cudaFree(numbers_d);
-    // printf("\n");
 }
 
 void print_sort_status(std::vector<int> numbers)
 {
     std::cout << "The input is sorted?: " << (std::is_sorted(numbers.begin(), numbers.end()) == 0 ? "False" : "True") << std::endl;
-}
-
-void print_array(std::vector<int> arr, int start, int stop) {
-    for (int i = start; i < stop; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
 }
 
 int main()
@@ -71,11 +61,9 @@ int main()
     std::generate(numbers.begin(), numbers.end(), rand);
 
     print_sort_status(numbers);
-    // print_array(numbers, 0, 100);
     auto start = std::chrono::steady_clock::now();
     oddeven_sort(numbers);
     auto end = std::chrono::steady_clock::now();
-    // print_array(numbers, 0, 100);
     print_sort_status(numbers);
     std::cout << "Elapsed time =  " << std::chrono::duration<double>(end - start).count() << " sec\n";
 }
